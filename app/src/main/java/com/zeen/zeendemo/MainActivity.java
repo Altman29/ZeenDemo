7847533b4777fbe09e71b8fb54684a9179274f6a
altman29 <s1yuan_chen@163.com>
2021-05-07 16:57:50 +0800

    feat: line chart view base.

--- app/src/main/java/com/zeen/zeendemo/MainActivity.java
+++ app/src/main/java/com/zeen/zeendemo/MainActivity.java
@@ -6,20 +6,21 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
+import androidx.appcompat.app.AppCompatActivity;
+
 import com.zeen.zeendemo.answer_views.AnswerViewsActivity;
 import com.zeen.zeendemo.bottomnav.BottomNavActivity;
 import com.zeen.zeendemo.countrypicker.CountryActivity;
 import com.zeen.zeendemo.fragmentVpFragment.Fvpf;
 import com.zeen.zeendemo.horizontal_lineview.NewLineActivity;
 import com.zeen.zeendemo.horizontallist.HorizontalRvActivity;
+import com.zeen.zeendemo.linechart_recently.LCVActivity;
 import com.zeen.zeendemo.linechartview.LineChartActivity;
 import com.zeen.zeendemo.mpchart.LineChartDemo;
 import com.zeen.zeendemo.ninepath.NinePathActivity;
 import com.zeen.zeendemo.pieview.PieChartActivity;
 import com.zeen.zeendemo.scroll_rv.ScrollRvActivity;
 import com.zeen.zeendemo.tracerv.TimeLineActivity;
-
-import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -113,5 +114,10 @@ public class MainActivity extends AppCompatActivity {
 
     public void goNavActivity(View view) {
         startActivity(new Intent(this, BottomNavActivity.class));
+    }
+
+
+    public void goRLVActivity(View view) {
+        startActivity(new Intent(this, LCVActivity.class));
     }
 }
